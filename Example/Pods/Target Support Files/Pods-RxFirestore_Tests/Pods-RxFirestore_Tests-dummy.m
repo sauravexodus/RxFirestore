dcf90853e804c252fc3ebe93249f90de510bfce6
Sourav Chandra <saurav.chandra1992@live.com>
2017-11-27 17:59:55 +0530

    Deleted pods

--- Example/Pods/Target Support Files/Pods-RxFirestore_Tests/Pods-RxFirestore_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxFirestore_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_RxFirestore_Tests
-@end
--- Example/Pods/Target Support Files/RxFirestore/RxFirestore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxFirestore : NSObject
-@end
-@implementation PodsDummy_RxFirestore
-@end
